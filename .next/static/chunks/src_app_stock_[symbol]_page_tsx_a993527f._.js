d2283c09dd5b0cca6daf098bf4a25940312d326c
trialvikas <161275143+trialvikas@users.noreply.github.com>
2025-08-12 20:17:01 +0530

    Add .next/static/chunks/src_app_stock_[symbol]_page_tsx_a993527f._.js 1755010020300

---
+++ .next/static/chunks/src_app_stock_[symbol]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_552e25f0._.js",
+  "static/chunks/src_fdaf76f5._.js"
+],
+    source: "dynamic"
+});
